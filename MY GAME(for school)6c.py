b4c47ff692c1c204e18a704c704b24c88268c075
isaacMullen <43121643+isaacMullen@users.noreply.github.com>
2018-09-21 14:24:45 -0300

    Add files via upload

--- MY GAME(for school)6c.py
+++ MY GAME(for school)6c.py
@@ -24,10 +24,10 @@ travel = (input('type "go" to travel to the fOrEsT oF tHe DeAd: '))
 
 if (travel == 'go'):
     print('travel will commence in...')
-    countdown(5)
+    countdown(1)
     print('[New Zone Discovered],fOrEsT oF tHe DeAd')
-    print('''You arrive at the Inn, located in the middle of the fOrEsT oF tHe DeAd. You approach an old man, sitting by the mailbox.
-It's unsettlingly dark and the air is occupied only by the noise of crows, They seem to be nervous over your arival...''')
+    print('''You arrive at the Inn, located in the middle of the FoReSt. You approach an old man, sitting by the mailbox.
+It's unsettlingly dark and the air is occupied only by the noise of crows, They seem to be nervous over your arrival...''')
 
 interact(playerCharacter, oldMan)
 
@@ -38,7 +38,39 @@ oldMan = 'Deccard'
 address = (input('type "excuse me, Deccard" to interact with the old man:'))
 
 if (address == "excuse me, Deccard"):
-    print('It is I, Deccard cain. What brings you to this vile, infested forest')
+    print('[Deccard] It is I, Deccard cain. What brings you to this vile, infested forest?')
+
+choice = int(input('''enter 1 to tell the truth
+enter 2 to lie: '''))
+
+
+if (choice == 1):
+    print('I have been sent by a squadrent located about 5 hours in that direction, you point south')
+    print('[Deccard] Ah yes, you must be',playerCharacter,',we couldn\'t be in more dire need of your help than we are now, we\'re being overrun by the undead')
+    print('[you] That is indeed my name, I am ready to begin my adventure at once')
+    print('[Deccard] wonderful, tell me when you want to start')
+    startAdventure = (input('type "begin" to embark on your adventure'))
+elif(choice == 2):
+    print('I wandered off my path on the way home and now now i\'ve ended up here')
+    print('[Deccard] Well if you\'re willing to stay and help us with the undead, I will reward you handsomely')
+    print('[you] I,',playerCharacter,', shall stay and help aid you in your efforts, I am always up for an adventure')
+    print('[Deccard] wonderful, tell me when you\'re prepared to start')
+    startAdventure = (input('type "begin" to embark on your adventure'))
+
+if (startAdventure == 'begin'):
+    print('''[daggers] ¤=[]:::::>
+¤=[]:::::>''')
+    print('''[wand + book] ━━━★
+ ___
+|   |
+| M |
+|___|''')
+    print('[sword] ¤==[]::::::::::::>')
+    print('[staff] 0===========O')
+    print(input('which weapon do you prefer?: '))
+
+
+                
 
 
 
